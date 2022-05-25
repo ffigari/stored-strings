255dca765265242a2b2c88450262c6042652e947
Francisco Figari <figafran@gmail.com>
2022-05-25 17:56:57 -0300

    Correct deploy script

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 lsof -ti:3000 && kill $(lsof -ti:3000)
-nohup node runner.js &
+nohup node runner.js spawn &
 
 NGINX_CONFIG_PATH=/etc/nginx/sites-enabled/stored-strings
 rm -rf $NGINX_CONFIG_PATH
