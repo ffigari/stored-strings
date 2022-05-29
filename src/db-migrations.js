cdc478528431ea0b50076e2a8bc12e98a336d0a5
Francisco Figari <figafran@gmail.com>
2022-05-29 12:58:06 -0300

    Create migrations table along db

--- src/db-migrations.js
+++ src/db-migrations.js
@@ -19,8 +19,6 @@ const getDBClient = async (connectionString, dbName) => {
 }
 
 module.exports.migrateDB = async (connectionString, dbName) => {
-  // TODO: Migrations should consider the possibility of the db being already
-  //       migrated
   let dbClient;
   try {
     dbClient = await getDBClient(connectionString, dbName);
@@ -28,23 +26,10 @@ module.exports.migrateDB = async (connectionString, dbName) => {
     if (e.code === '3D000') {  // If DB does not exist
                                // https://www.postgresql.org/docs/current/errcodes-appendix.html
       const defaultDBClient = await getDefaultDBClient(connectionString);
+
       await defaultDBClient.query(`create database ${dbName}`);
       dbClient = await getDBClient(connectionString, dbName);
-    } else {
-      throw e;
-    }
-  }
-  let lastRunMigration;
-  try {
-    lastRunMigration = (await dbClient.query(`
-      select name
-      from migrations
-      order by application_order desc
-      limit 1
-    `)).rows[0].name
-  } catch (e) {
-    // TODO: Maybe this should be done along with the db creation
-    if (e.code === '42P01') {  // If table does not exist
+
       await dbClient.query(`
         create table migrations (
           name text not null check (name <> ''),
@@ -52,29 +37,32 @@ module.exports.migrateDB = async (connectionString, dbName) => {
           application_order serial,
           unique (name)
         )
-      `)
-      const dbCreationMigrationName = 'creation';
-      await dbClient.query(`
-        insert into migrations values ('${dbCreationMigrationName}')
-      `)
-      lastRunMigration = dbCreationMigrationName;
+      `);
+      await dbClient.query(`insert into migrations values ('creation')`);
     } else {
-      throw e
+      throw e;
     }
   }
-  // Migrations should be run inside a transaction
-  console.log(`TODO: Verify migrations status, last migration run=${lastRunMigration}`)
-  console.log(`TODO: Run '${dbName}'\'s migrations`)
+  const lastRunMigration = (await dbClient.query(`
+      select name
+      from migrations
+      order by application_order desc
+      limit 1
+    `)).rows[0].name;
+
+  // TODO: Migrate up or down
+  //   - run each m inside a tx
+  //   - before running them, verify run migrations are consistent with input
+  //     migrations; not sure which verifications have to be done
+  //
+  // How do I ensure migrations are reversible? A pre-commit hook which goes up
+  // and down with a development db?
   throw `'migrateDB' not implemented`
 }
 
 module.exports.ensureDBState = async (connectionString, dbName) => {
   const client = await getDBClient(connectionString, dbName);
-  // Maybe the best here is to simply check that the last run migration
-  // coincides with the last one created
-  // But how are migration identified: an id? how do I choose it? manually? What
-  // about git merges? Maybe just a name and doing a check that names do not
-  // repeat? When, before running migrations? With a pre-commit hook?
+  // TODO: Check migrations are up to date
   console.log(await client.query('select now()'))
   throw `'ensureDBState' not implemented`
 }
